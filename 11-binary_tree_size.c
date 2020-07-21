fe4136edc6fc65bf905cbcfc62b1be656d3197a4
fernandoSandoval <sandovbarr@gmail.com>
2020-07-21 12:17:00 -0500

    [FIX] betty codestyle

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -14,8 +14,8 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-        size++;
-        size += binary_tree_size(tree->left);
-        size += binary_tree_size(tree->right);
-        return (size);
+	size++;
+	size += binary_tree_size(tree->left);
+	size += binary_tree_size(tree->right);
+	return (size);
 }
