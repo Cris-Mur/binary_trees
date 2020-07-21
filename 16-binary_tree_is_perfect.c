45223a161e33031b3d65e98b383323a2c068c079
fernandoSandoval <sandovbarr@gmail.com>
2020-07-21 18:39:09 -0500

    [FIX] betty codestyle

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -14,12 +14,10 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-        if (binary_tree_nodes(tree->left) == binary_tree_nodes(tree->right))
-        {
-                if (binary_tree_leaves(tree->left) == binary_tree_leaves(tree->right))
-                {
-                        return (1);
-                }
-        }
-        return (0);
+	if (binary_tree_nodes(tree->left) == binary_tree_nodes(tree->right))
+	{
+		if (binary_tree_leaves(tree->left) == binary_tree_leaves(tree->right))
+			return (1);
+	}
+	return (0);
 }
