ed69c5dfbf91d9d8ab2feca753e8b515726b8bed
fernandoSandoval <sandovbarr@gmail.com>
2020-07-21 11:01:20 -0500

    [ADD] betty style

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,6 +10,7 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node = NULL;
+
 	node = malloc(sizeof(binary_tree_t));
 	if (!node)
 		return (NULL);
@@ -20,5 +21,5 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	if (parent)
 		node->parent = parent;
-	return node;
+	return (node);
 }
