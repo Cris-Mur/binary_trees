e61d26351ef2d9b7128b832352f05b79fb587c6a
Cris-Mur <yomy98@gmail.com>
2020-07-22 19:11:37 -0500

    [ancestor V1.0]

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -17,7 +17,7 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tr
         binary_tree_t **l_P1 = NULL, **l_P2 = NULL;
         if (!first || !second)
                 return (NULL);
-        
+
         printf("FIRST\n[%d]\n", first->n);
         l_P1 = l_parents((binary_tree_t *)first);
         printf("&l_P1: %p\n", (void *)l_P1);
@@ -25,15 +25,17 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tr
         l_P2 = l_parents((binary_tree_t *)second);
         printf("&l_P2: %p\n", (void *)l_P2);
 
-        n1 = (sizeof(binary_tree_t **)/sizeof(l_P1[0]));
+        n1 = binary_tree_depth(first) + 1;
         printf("%lu\n", n1);
-        n2 = (sizeof(binary_tree_t **)/sizeof(l_P2[0]));
+        n2 = binary_tree_depth(second) + 1;
         printf("%lu\n", n2);
         for (i = 0; i < n1; i++)
                 for (j = 0; j < n2; j++)
-                        printf("%d == %d ?", l_P1[i]->n, l_P2[j]->n);
-                        if (l_P1[i] == l_P2[j])
+		{
+                        printf("%d == %d ?\n", l_P1[i]->n, l_P2[j]->n);
+			if (l_P1[i] == l_P2[j])
                                 return (l_P1[i]);
+		}
         return (NULL);
 }
 
@@ -41,13 +43,13 @@ binary_tree_t **l_parents(binary_tree_t *node)
 {
         binary_tree_t **l_P = NULL;
         int N = 0, i;
-        
+
         N = binary_tree_depth(node) + 1;
-        
+
         l_P = malloc(N * sizeof(binary_tree_t *));
         if (!l_P)
                 return (NULL);
-        
+
         l_P[0] = node;
         for (i = 1; i < N && node->parent; i++)
         {
